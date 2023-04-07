ce20b777bbb5de49ceb06a588dc11af6086f5818
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2023-04-07 20:26:48 +0100

    finished off modifying buttons in form

--- login-react-page/src/components/Form.js
+++ login-react-page/src/components/Form.js
@@ -2,40 +2,58 @@ import React from 'react'
 
 function Form() {
   return (
-    <div>
-        <h1>Welcome back</h1>
-        <p>Please enter your details </p>
+    // Px is padding in the x-axis and py is padding in the y-axis
+    <div className='form-container border-2 bg-white px-10 py-20 rounded-3xl border-gray-200'>
+        <h1 className="text-5xl font-semibold">Welcome back</h1>
+        <p className="font-medium text-lg text-gray-500 mt-4">Please enter your details </p>
 
-        <form>
+        <form className="mt-8  ">
             <div>
-                <label>Email</label>
+                <label className="text-lg font-medium">Email</label>
                 <input 
-                className='' 
+                className='w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent' 
                 placeholder='Enter email'
                 type='text'
                 />
             </div>
             <div>
-                <label>Password</label>
+                <label className="text-lg font-medium">Password</label>
                 <input 
-                className='' 
+                className='w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent' 
                 placeholder='Enter password'
                 type='password'
                 />
             </div>
-            <div className='div for buttons'>
-                <div className="checkbox">
+            <div className='mt-8 flex justify-between items-center'>
+                <div>
                     <input
                         type="checkbox"
                         id='remember'
                      />
-                     <label for="remember">Remeber password</label> 
+                     {/* remeber the lable is the text that goes next to the checkbox here or next to the input box etc 
+                     text-base is the default size for text*/}
+                     <label className='ml-2 font-medium text-base ' for="remember">Remeber password</label> 
+                     <button className=" text-base font-medium text-violet-500 ml-2">Forgot password </button>
+                     {/* Separation on the y axis of 4 (gap-y-4) */}
+                    <div className='mt-8 flex flex-col gap-y-4'>
+                        <button className="bg-violet-500 rounded-xl text-white text-lg font-bold py-3 active:scale-[.98] transition-all active:duration-75 hover:bg-blue-500 hover:scale-[1.01] ease-in-out">Sign in</button>
+                        <button className='flex border-2 rounded-xl items-center justify-center gap-2 text-black text-lg font-bold rounded py-3 active:scale-[.98] transition-all active:duration-75 hover:bg-gray-200 hover:scale-[1.01] ease-in-out'>
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M5.26644 9.76453C6.19903 6.93863 8.85469 4.90909 12.0002 4.90909C13.6912 4.90909 15.2184 5.50909 16.4184 6.49091L19.9093 3C17.7821 1.14545 15.0548 0 12.0002 0C7.27031 0 3.19799 2.6983 1.24023 6.65002L5.26644 9.76453Z" fill="#EA4335"/>
+                                <path d="M16.0406 18.0142C14.9508 18.718 13.5659 19.0926 11.9998 19.0926C8.86633 19.0926 6.21896 17.0785 5.27682 14.2695L1.2373 17.3366C3.19263 21.2953 7.26484 24.0017 11.9998 24.0017C14.9327 24.0017 17.7352 22.959 19.834 21.0012L16.0406 18.0142Z" fill="#34A853"/>
+                                <path d="M19.8342 20.9978C22.0292 18.9503 23.4545 15.9019 23.4545 11.9982C23.4545 11.2891 23.3455 10.5255 23.1818 9.81641H12V14.4528H18.4364C18.1188 16.0119 17.2663 17.2194 16.0407 18.0108L19.8342 20.9978Z" fill="#4A90E2"/>
+                                <path d="M5.27698 14.2663C5.03833 13.5547 4.90909 12.7922 4.90909 11.9984C4.90909 11.2167 5.03444 10.4652 5.2662 9.76294L1.23999 6.64844C0.436587 8.25884 0 10.0738 0 11.9984C0 13.918 0.444781 15.7286 1.23746 17.3334L5.27698 14.2663Z" fill="#FBBC05"/>
+                            </svg>
+                            Sign in with Google</button>
+                    </div>
+                    <div className='flex items-center justify-center font-medium text-base mt-4'>
+                        <p>Don't have an account?</p>
+                        <button className='text-violet-500 ml-2'>Sign up</button>
+                    </div>
                 </div>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Forgot password </button>
-            </div>
+               
 
-        
-            
+            </div>   
         </form>
     </div>
   )
