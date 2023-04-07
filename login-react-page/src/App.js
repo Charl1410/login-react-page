ecfc34ac91ad7675a4b229d4c238a788bcd783b0
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2023-04-07 19:20:24 +0100

    position and animation to circle

--- login-react-page/src/App.js
+++ login-react-page/src/App.js
@@ -9,10 +9,15 @@ function App() {
       </div>
     {/* The hidden origionally hides this div as we don't want it in mobile view but when 
     the screen is large then the display is set to flex so we can see it */}
-      <div className="hidden lg:flex h-full w-1/2 bg-gray-200 items-center justify-center ">
+    {/* Set relative for this parent of the child that has absolute to it  */}
+      <div className="hidden relative lg:flex h-full w-1/2 bg-gray-200 items-center justify-center ">
         {/* This color styling 'bg-gradient-to-tr from-violet-500 to-pink-500'creates bottom right to top left   */}
-        <div className='w-60 h-60 bg-gradient-to-tr from-violet-500 to-pink-500 rounded-full'></div>
+        {/* This is the circle.  */}
+        <div className='w-60 h-60 bg-gradient-to-tr from-violet-500 to-pink-500 rounded-full animate-spin'></div>
+        {/* The position absolute will refer to the parent relative to position itself*/}
+        <div className='w-full h-1/2 absolute bottom-0 bg-white/10 backdrop-blur-lg '></div>
       </div>
+
     </div>
   );
 }
