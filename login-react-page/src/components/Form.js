20dfa30e8ec11ea20f12123f3875226345d74d6b
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2023-04-07 19:31:17 +0100

    html form

--- login-react-page/src/components/Form.js
+++ login-react-page/src/components/Form.js
@@ -2,7 +2,42 @@ import React from 'react'
 
 function Form() {
   return (
-    <div>Form</div>
+    <div>
+        <h1>Welcome back</h1>
+        <p>Please enter your details </p>
+
+        <form>
+            <div>
+                <label>Email</label>
+                <input 
+                className='' 
+                placeholder='Enter email'
+                type='text'
+                />
+            </div>
+            <div>
+                <label>Password</label>
+                <input 
+                className='' 
+                placeholder='Enter password'
+                type='password'
+                />
+            </div>
+            <div className='div for buttons'>
+                <div className="checkbox">
+                    <input
+                        type="checkbox"
+                        id='remember'
+                     />
+                     <label for="remember">Remeber password</label> 
+                </div>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Forgot password </button>
+            </div>
+
+        
+            
+        </form>
+    </div>
   )
 }
 
