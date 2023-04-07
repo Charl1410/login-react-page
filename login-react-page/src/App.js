f9c686988a4bb8f4cbd31fe1dc629fd2205766da
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2023-04-07 19:10:13 +0100

    form on one side with circle on other in lg view

--- login-react-page/src/App.js
+++ login-react-page/src/App.js
@@ -3,14 +3,15 @@ import Form from './components/Form'
 
 function App() {
   return (
-    <div className="flex w-full h-screen flex items-center justify-center lg:w-1/2">
-      <div>
+    <div className="flex w-full h-screen ">
+      <div className='w-full flex items-center justify-center lg:w-1/2'>
         <Form />
       </div>
     {/* The hidden origionally hides this div as we don't want it in mobile view but when 
     the screen is large then the display is set to flex so we can see it */}
-      <div className="hidden lg:flex h-full bg-gray-200">
-        <div>Hello</div>
+      <div className="hidden lg:flex h-full w-1/2 bg-gray-200 items-center justify-center ">
+        {/* This color styling 'bg-gradient-to-tr from-violet-500 to-pink-500'creates bottom right to top left   */}
+        <div className='w-60 h-60 bg-gradient-to-tr from-violet-500 to-pink-500 rounded-full'></div>
       </div>
     </div>
   );
